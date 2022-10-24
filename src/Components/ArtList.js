8e7496a813134ffa94dfd461870e5f036fd888e6
Jihun Kim <zh1229@naver.com>
2022-10-24 20:46:13 +0900

    add: JSON file

--- src/Components/ArtList.js
+++ src/Components/ArtList.js
@@ -1,10 +1,8 @@
 import { Routes, Route, Link, useNavigate, Outlet } from "react-router-dom";
 import { Button, Navbar, Container, Nav } from "react-bootstrap";
-import artpiece from "./artpiece";
+import db from "../sample_data.json"
 
 function ArtList(props) {
-
-
     return(
         <div class="container">
 					<header className="major">
@@ -14,19 +12,18 @@ function ArtList(props) {
                         <h3>Art collections</h3>
                         <div className="box alt">
                             <div className="row gtr-50 gtr-uniform">
-                                {props.artpiece.map((item, index) => {
+                                {db.map((item, index) => {
                                     return(
                                             <div className="col-4 col-6-xsmall" key={index}>
-                                                <Nav.Link href={'detail/'+item.id}>
-                                                    <img src={item.src} width="90%" />
+                                                <Nav.Link href={'detail/'+item.Obj_ID}>
+                                                    <img src={item.Img_file} width="90%" />
                                                 </Nav.Link>
-                                                <h4>id : {item.id}</h4>
-                                                <h4>{item.title}</h4>
-                                                <h4>{item.est}</h4>
-                                                <h4>{item.size}</h4>
-                                                <h4>{item.style}</h4>
-                                                <h4>{item.name}</h4>
-                                                <h4>{index}</h4>
+                                                <h4>key: {index}</h4>
+                                                <h4>Obj_ID : {item.Obj_ID}</h4>
+                                                <h4>{item.Title}</h4>
+                                                <h4>{item.Obj_dates}</h4>
+                                                <h4>{item.Dimensions}</h4>
+                                                <h4>{item.Mediums}</h4>
                                             </div>
                                     );
                                 })};
--- src/Components/Detail.js
+++ src/Components/Detail.js
@@ -5,15 +5,14 @@ import Rating from "./rating";
 import art1 from "./img/art1.png"
 
 import { useParams } from "react-router-dom";
-import artpiece from "./artpiece";
+import db from "../sample_data.json"
 
-
-function Detail(props) {
+function Detail() {
 
     const sizeText = {
         display: "flex",
         justifyContent: "center",
-        alignItems: "center",
+        aligndbs: "center",
         
     }
     const imgsize = {
@@ -23,13 +22,14 @@ function Detail(props) {
         // paddingTop: "20px",
         display: "flex",
         justifyContent: "center",
-        alignItems: "center"
+        aligndbs: "center"
     }
 
     const { id } = useParams();
-    const artpiece = props.artpiece.filter((artpiece) => artpiece.id === Number(id));
+    // const db = props.db.filter((db) => db.id === Number(id));
+    
 
-    if (artpiece.lenth === 0) {
+    if (db.lenth === 0) {
         return(
             <div>
                 <h3>존재하지 않는 작품입니다.</h3>
@@ -40,36 +40,40 @@ function Detail(props) {
     return(
         <div id="main" className="wrapper style1">
             <div className="row gtr-150">
-                <div className="col-8 col-12-medium"></div>
-                    <div>
-                            <section id="content" style={imgsize}>
-                                <a className="image fit"><img src={props.artpiece[id].src}/></a>
-                            </section>
-                    </div>
-                    <div>
-                            <section id="sidebar">
-                                <section>
-                                    <h3 className="align-center">{props.artpiece[id].title}</h3>
-                                    {/* <h3>현재 페이지의 id는 {id}입니다.</h3> */}
-                                    <h3 className="align-center">코튼에 유채, 235 x 183cm</h3>
-                                    <h3 className="align-center">{props.artpiece[id].style}, {props.artpiece[id].size}</h3>
-                                    <h3 className="align-center">김환기</h3>
-                                    <h3 className="align-center">{props.artpiece[id].name}</h3>
-                                    <h3 className="align-center">1978</h3>
-                                    <h3 className="align-center">{props.artpiece[id].est}</h3>
-                                    <div style={sizeText}><Rating></Rating></div>
-                                    &nbsp;
-                                    <div className="row.gtr-150">
-                                        <p style={sizeText}>김환기는 미국으로 건너간 1963년부터 작고한 1974년 이르는 뉴욕시대를 통해 1950년 후반부터 산, 달, 강, 새, 나무 등이 있는 자연 풍경을 순수한 점, 선, 면의 조형적 요소로써 내밀한 서정의 새계로 심화시켰습니다.</p>
-                                        <p style={sizeText}>1968년 1월 23일 작가의 일기에 "날으는 점, 점들이 모여 형태를 상징하는 그런 것들을 시도하다."라고 적혀 있습니다. 이러한 점 모티브는 뉴욕시대에 행해진 과슈와 콜라주 그리고 파피에 마쉐, 오브제, 종이에 유채, 드로잉 등과 같이
-                                            재료와 양식의 범주를 자유롭게 넘나들었던 다양한 조형실험을 거쳐 70년대에 들어서면서 화면 전체를 덮는 전면점화로 발전됩니다.	
-                                        </p>
+                        {db.map((item, index) => {
+                            return(
+                                <div className="col-8 col-12-medium" key={index}>
+                                    <div>
+                                        <section id="content" style={imgsize}>
+                                            <a className="image fit"><img src={item.Img_file}/></a>
+                                        </section>
                                     </div>
-                                </section>
-                            </section>
-                    </div>
-                </div>
+                                    <div>
+                                        <section id="sidebar" key={index}>
+                                            <section>
+                                                <h3 className="align-center">{item.Title}</h3>
+                                                <h3 className="align-center">코튼에 유채, 235 x 183cm</h3>
+                                                <h3 className="align-center">{item.Mediums}, {item.Dimensions}</h3>
+                                                <h3 className="align-center">김환기</h3>
+                                                <h3 className="align-center">1978</h3>
+                                                <h3 className="align-center">{item.Obj_dates}</h3>
+                                                <h3>{item.index}</h3>
+                                                <div style={sizeText}><Rating></Rating></div> 
+                                                &nbsp;
+                                                <div className="row.gtr-150">
+                                                    <p style={sizeText}>김환기는 미국으로 건너간 1963년부터 작고한 1974년 이르는 뉴욕시대를 통해 1950년 후반부터 산, 달, 강, 새, 나무 등이 있는 자연 풍경을 순수한 점, 선, 면의 조형적 요소로써 내밀한 서정의 새계로 심화시켰습니다.</p>
+                                                    <p style={sizeText}>1968년 1월 23일 작가의 일기에 "날으는 점, 점들이 모여 형태를 상징하는 그런 것들을 시도하다."라고 적혀 있습니다. 이러한 점 모티브는 뉴욕시대에 행해진 과슈와 콜라주 그리고 파피에 마쉐, 오브제, 종이에 유채, 드로잉 등과 같이
+                                                        재료와 양식의 범주를 자유롭게 넘나들었던 다양한 조형실험을 거쳐 70년대에 들어서면서 화면 전체를 덮는 전면점화로 발전됩니다.	
+                                                    </p>
+                                                </div>
+                                            </section>
+                                        </section>
+                                    </div>
+                                </div>
+                            );
+                        })};
             </div>
+       </div>
     )
 }
 
